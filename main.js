fc391e78298b63c6d4aa644f100530742b30bb51
vishal <vishalninaniya8949info@gmail.com>
2024-10-16 13:37:36 +0530

    updating adding javascript

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-const colorPicker = document.querySelector("c--picker");
+const colorPicker = document.querySelector(".c--picker");
 const imageWidth = document.querySelector("#width");
 const imageHeight = document.querySelector("#height");
 const canva = document.querySelector("#canvas");
+const body = document.querySelector("body");
 
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const width = imageWidth.value;
+  const height = imageHeight.value;
+  const color = colorPicker.value;
 });
-
-const pickr = new Pickr.create({
-  el: ".color-picker",
-  theme: "classic",
-  swatches: [],
-  useAsButton: false,
-  default: "#3498db",
-  components: {
-    preview: false,
-    opacity: true,
-    hue: true,
-    interaction: {
-      hex: true,
-      rgba: true,
-      hsla: false,
-      hsva: false,
-      cmyk: false,
-      input: true,
-      clear: false,
-      save: false,
-    },
-  },
+// colorPicker.addEventListener("change", (e) => {
+//   console.log("changed");
+// });
+colorPicker.addEventListener("input", (e) => {
+  body.style.backgroundColor = colorPicker.value;
 });
